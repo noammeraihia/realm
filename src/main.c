5d4c12399269ad4a0b1c6e1f0411cbc33f620130
noammeraihia <meraihianoam@gmail.com>
2025-05-29 14:37:12 +0200

    build system and project setup

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mdl.h>
+#include <SDL3/SDL.h>
+
+int main(int argc, char* argv[])
+{
+        mdl_LOG_INFO("\n", "Welcome to Realm!");
+
+        return 0;
+}
